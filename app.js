4702709018108e1ebda04785af0a1997a56d8917
abdelaziz-93 <abdelaziz.oubella93@gmail.com>
2024-05-23 11:06:29 +0100

    commit

--- app.js
+++ app.js
@@ -270,5 +270,19 @@ btn2.forEach(button => {
   });
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+
 
 
